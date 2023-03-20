b29900622e10acb8b4fc2175f88332c3bd246549
Tomasz Ż <tzuber95@gmail.com>
2023-03-20 15:57:23 +0100

    start styling add task container

--- src/components/todolist/AddTodoBox.js
+++ src/components/todolist/AddTodoBox.js
@@ -1,5 +1,20 @@
+import styles from './AddTodoBox.module.css'
+
 const AddTodoBox = () => {
-    return <p>TodoBox</p>
+	return (
+		<div className={styles.container}>
+			<label htmlFor='task' className={styles.label}>
+				Add task to the list
+			</label>
+			<p className={styles.error}>Thie field cannot be empty!</p>
+			<div className={styles.box}>
+				<input type='text' id='task' placeholder='Enter the content of the task...' className={styles.input} />
+				<button aria-label='Add new task' className={styles.button}>
+					Add
+				</button>
+			</div>
+		</div>
+	)
 }
 
 export default AddTodoBox
